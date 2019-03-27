f73e9a1544a9f01781a465a94a03e39f41ee08f5
Den Dodge <nestruev98@mail.ru>
2019-03-27 13:06:41 +0300

    add new project

---
+++ server/netserver.c
@@ -0,0 +1,150 @@
+/*
+** server.c -- a stream socket server demo
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+#define BUF_SIZE 9999
+
+#define PORT "3490"  // порт, на который будут приходить соединения
+
+#define BACKLOG 10     // как много может быть ожидающих соединений
+
+void sigchld_handler(int s)
+{
+    while(waitpid(-1, NULL, WNOHANG) > 0);
+}
+
+// получаем адрес сокета, ipv4 или ipv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int main(void)
+{
+    int sockfd, new_fd;  // слушаем на sock_fd, новые соединения - на new_fd
+    struct addrinfo hints, *servinfo, *p;
+    struct sockaddr_storage their_addr; // информация об адресе клиента
+    socklen_t sin_size;
+    struct sigaction sa;
+    int yes=1;
+    char s[INET6_ADDRSTRLEN];
+    int rv;
+    char bufin[BUF_SIZE];
+    char bufout[BUF_SIZE];
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE; // use my IP
+
+    if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s \n", gai_strerror(rv));
+        return 1; 
+    }
+
+    // цикл через все результаты, чтобы забиндиться на первом возможном
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            perror("server: socket \n");
+            continue;
+        }
+
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+                sizeof(int)) == -1) {
+            perror("setsockopt \n");
+            exit(1);
+        }
+
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("server: bind \n");
+            continue;
+        }
+
+        break;
+    }
+
+    if (p == NULL)  {
+        fprintf(stderr, "server: failed to bind \n");
+        return 2;
+    }
+
+    freeaddrinfo(servinfo); // всё, что можно, с этой структурой мы сделали
+
+    if (listen(sockfd, BACKLOG) == -1) {
+        perror("listen \n");
+        exit(1);
+    }
+
+    sa.sa_handler = sigchld_handler; // обрабатываем мёртвые процессы
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+        perror("sigaction \n");
+        exit(1);
+    }
+
+    printf("server: waiting for connections... \n");
+
+    while(1) {  // главный цикл accept()
+        sin_size = sizeof their_addr;
+        new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+        if (new_fd == -1) {
+            perror("accept \n");
+            continue;
+        }
+
+        inet_ntop(their_addr.ss_family,
+            get_in_addr((struct sockaddr *)&their_addr),
+            s, sizeof s);
+        printf("server: got connection from %s \n", s);
+       
+	send(new_fd, "Enter the number.\n1-'HI!'\n2-'My name is ...'\n", BUF_SIZE-1, 0);
+
+	while(1)
+	{
+		memset(bufin, 0, BUF_SIZE);
+		read(new_fd, bufin, BUF_SIZE-1);
+		bufin[BUF_SIZE] = 0;
+		printf("MSG: %s\n", bufin);
+		switch(bufin[0]){
+			case '1': send(new_fd, "hi!", BUF_SIZE-1, 0);
+				  break;
+			case '2': send(new_fd, "Nice to meet you! My name is server", BUF_SIZE-1, 0);
+				  break;
+			default: send(new_fd, "not command", BUF_SIZE-1, 0);
+				 break;
+		}
+	}
+
+        //if (!fork()) { // тут начинается дочерний процесс
+            //close(sockfd); // дочернему процессу не нужен слушающий сокет
+            //if (send(new_fd, "Hello, world!", 13, 0) == -1)
+                //perror("send \n");
+            //close(new_fd);
+            //exit(0);
+	//}
+	close(sockfd);
+        close(new_fd);  // а этот сокет больше не нужен родителю
+    }
+
+    return 0;
+}
